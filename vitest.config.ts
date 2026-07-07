e4bf1a2885ddc597ffdf5b9c10bb652b481ff33b
Marco André Mendes <marcoandre@gmail.com>
2026-07-07 10:32:08 -0300

    chore: configure ci and test coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,17 +1,33 @@
 import { defineConfig } from 'vitest/config'
 import vue from '@vitejs/plugin-vue'
-import { resolve } from 'path'
 
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src'),
-    },
-  },
+  plugins: [vue],
+
   test: {
     environment: 'jsdom',
-    globals: true,
-    include: ['src/**/*.test.ts', 'src/**/*.spec.ts'],
-  },
+
+    coverage: {
+      enabled: true,
+      provider: 'v8',
+
+      reporter: [
+        'text',
+        'html',
+        'lcov'
+      ],
+
+      reportsDirectory: './coverage',
+
+      exclude: [
+        'node_modules/',
+        'dist/',
+        'coverage/',
+        'src/tests/',
+        '**/*.d.ts',
+        '**/*.config.*',
+        '**/main.ts'
+      ]
+    }
+  }
 })
